3830568e3af2497ee5b467e2484873539da3cf42
Jhony Smith <devJonatasSilva@gmail.com>
2021-10-24 19:03:16 -0300

    adicionando o projeto introdução ao JUnit5

---
+++ introducao ao JUnit5/usandoJUnit5/src/main/java/calculadoraSimples/Calculadora.java
@@ -0,0 +1,25 @@
+package calculadoraSimples;
+
+/**
+ *
+ * @author jony_
+ */
+public class Calculadora {
+    
+    public double soma(double parcela1, double parcela2){
+        return parcela1 + parcela2;
+    }
+    
+    public double subtracao(double minuendo, double subtraendo){
+        return minuendo - subtraendo;
+    }
+    
+    public double multiplicacao(double fator1, double fator2){
+        return fator1 * fator2;
+    }
+    
+    public double divisao(double dividendo, double divisor){
+        return dividendo / divisor;
+    }
+    
+}
